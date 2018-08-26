2cc67c65dceecb5f30f03becdf0db1f137054b1f
Naga <chchiang.cs03g@g2.nctu.edu.tw>
2018-08-26 23:43:44 +0800

    ProgressBar appearance

---
+++ Scripts/UI/ProgressBar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Timespawn.Core.UI
+{
+    public class ProgressBar : MonoBehaviour
+    {
+        [SerializeField] protected Image FillImage;
+    }
+}
