4295f6749bd309e4daef3f6d4f48a32b7c41a680
Naga <chchiang.cs03g@g2.nctu.edu.tw>
2018-09-08 23:13:58 +0800

    Basic progress bar

---
+++ Scripts/Demo/ProgressBarDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Timespawn.Core.UI;
+using UnityEngine;
+
+namespace Timespawn.Core.Demo
+{
+    public class ProgressBarDemo : MonoBehaviour
+    {
+        [Header("Parameters")]
+        [SerializeField] private float ValueMax = 100.0f;
+        [SerializeField] private Vector2 RandomValueRange = new Vector2(10.0f, 80.0f);
+
+        [Header("UI")]
+        [SerializeField] private ProgressBar Bar;
+
+        private float Value = 0.0f;
+
+        private void Awake()
+        {
+            Value = ValueMax;
+        }
+
+        private void Start()
+        {
+            Bar.SetPercent(Value / ValueMax);
+        }
+
+        public void IncreaseProgressButton_OnClick()
+        {
+            float valueChange = UnityEngine.Random.Range(RandomValueRange.x, RandomValueRange.y);
+            Value = Mathf.Clamp(Value + valueChange, 0.0f, ValueMax);
+            Bar.SetPercent(Value / ValueMax);
+        }
+
+        public void DecreaseProgressButton_OnClick()
+        {
+            float valueChange = UnityEngine.Random.Range(RandomValueRange.x, RandomValueRange.y);
+            Value = Mathf.Clamp(Value - valueChange, 0.0f, ValueMax);
+            Bar.SetPercent(Value / ValueMax);
+        }
+    }
+}
--- Scripts/UI/ProgressBar.cs
+++ Scripts/UI/ProgressBar.cs
@@ -7,6 +7,42 @@ namespace Timespawn.Core.UI
 {
     public class ProgressBar : MonoBehaviour
     {
+        [Header("Parameters")]
+        [SerializeField] protected float LerpDuration = 0.5f;
+
+        [Header("Images")]
         [SerializeField] protected Image FillImage;
+
+        protected float Percent;
+        protected float LastPercent;
+        protected float LerpStep;
+
+        protected virtual void Update()
+        {
+            UpdateFillAmount();
+        }
+
+        public float GetPercent()
+        {
+            return Percent;
+        }
+
+        public void SetPercent(float percent)
+        {
+            LastPercent = Percent;
+            Percent = percent;
+            LerpStep = 0.0f;
+        }
+
+        private void UpdateFillAmount()
+        {
+            if (LerpStep >= 1.0f)
+            {
+                return;
+            }
+
+            LerpStep += Time.deltaTime / LerpDuration;
+            FillImage.fillAmount = Mathf.Lerp(LastPercent, Percent, LerpStep);
+        }
     }
 }
