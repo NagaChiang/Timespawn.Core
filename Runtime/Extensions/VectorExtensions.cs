39109aef988f43c957930566768d0e3cd2e986f2
Naga <nagachiangdev@gmail.com>
2020-06-27 21:54:59 +0800

    Remove redundant vector conversion since mathematics package already supports

--- Runtime/Extensions/VectorExtensions.cs
+++ Runtime/Extensions/VectorExtensions.cs
@@ -5,31 +5,6 @@ namespace Timespawn.Core.Extensions
 {
     public static class VectorExtensions
     {
-        public static Vector2 ToVector(this float2 pos)
-        {
-            return new Vector2(pos.x, pos.y);
-        }
-
-        public static Vector3 ToVector(this float3 pos)
-        {
-            return new Vector3(pos.x, pos.y, pos.z);
-        }
-
-        public static Vector4 ToVector(this float4 pos)
-        {
-            return new Vector4(pos.x, pos.y, pos.z, pos.w);
-        }
-
-        public static Vector2Int ToVector(this int2 pos)
-        {
-            return new Vector2Int(pos.x, pos.y);
-        }
-
-        public static Vector3Int ToVector(this int3 pos)
-        {
-            return new Vector3Int(pos.x, pos.y, pos.z);
-        }
-
         public static float3 ToFloat3(this float2 pos)
         {
             return new float3(pos.x, pos.y, 0.0f);
